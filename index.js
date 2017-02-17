91ab03028dca879caa48c47a8c27492bfddeefe7
VanishingDante <VanishingDante@gmail.com>
2017-02-17 16:42:23 +0800

    update

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 #!/usr/bin/env node
+const { exec } = require('child_process')
+const fs = require('fs-extra')
 
-const exec = require('child_process').exec;
+let dirname = process.argv[2]
+if (!dirname) throw new Error('path required')
+let currentFolder = false
+if (dirname === '.') {
+  currentFolder = true
+  dirname = '@clone_react_template_temp'
+}
 
-const dirName = process.argv[2]
-
-exec(`git clone https://github.com/VanishingDante/react-template.git ${dirName}`, (error, stdout, stderr) => {
+exec(`git clone https://github.com/VanishingDante/react-start-template.git ${dirname}`, (error, stdout, stderr) => {
   if (error) {
     throw error
   }
 
-  exec(`rm -rf ./${dirName}/.git`, (error, stdout, stderr) => {
-    if (error) {
-      throw error
+  try {
+    fs.removeSync(`./${dirname}/.git`)
+  } catch (err) {
+    console.error(err)
+  }
+
+  if (currentFolder) {
+    try {
+      fs.copySync(dirname, '.')
+      fs.removeSync(dirname)
+    } catch (err) {
+      console.error(err)
     }
-  })
+  }
 })
